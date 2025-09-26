ee50174acc2ce2817c9a0302393f7bd5508ce17e
Alex <alex.m.guinea@gmail.com>
2025-09-26 08:26:02 +0200

    Changed train

--- train.py
+++ train.py
@@ -1,16 +1,26 @@
+import os
 import pandas as pd
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix
 import joblib
 import mlflow
 import mlflow.sklearn
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 
 # Cargar el conjunto de datos
-iris = datasets.load_iris()
-X = iris.data
-y = iris.target
+try:
+    iris = pd.read_csv("data/iris_dataset.csv")
+except FileNotFoundError:
+    print("Error: El archivo 'data/iris_dataset.csv' no fue encontrado.")
+
+# Dividir el DataFrame en características (X) y etiquetas (y)
+X = iris.drop('target', axis=1)
+y = iris['target']
+
 # Iniciar un experimento de MLflow
 with mlflow.start_run():
     # Dividir los datos en conjuntos de entrenamiento y prueba
@@ -19,7 +29,7 @@ with mlflow.start_run():
     )
 
     # Inicializar y entrenar el modelo
-    model = RandomForestClassifier(n_estimators=100, random_state=42)
+    model = RandomForestClassifier(n_estimators=200, random_state=42)
     model.fit(X_train, y_train)
 
     # Realizar predicciones y calcular la precisión
@@ -33,8 +43,8 @@ with mlflow.start_run():
     mlflow.sklearn.log_model(model, "random-forest-model")
 
     # Registrar parámetros y métricas
-    mlflow.log_param("n_estimators", 100)
+    mlflow.log_param("n_estimators", 200)
     mlflow.log_metric("accuracy", accuracy)
-    
+
     print(f"Modelo entrenado y precisión: {accuracy:.4f}")
     print("Experimento registrado con MLflow.")
